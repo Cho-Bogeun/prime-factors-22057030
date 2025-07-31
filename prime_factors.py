7aaf95e08714674730cce56d9d335bb8b9e59da7
Cho-Bogeun <bog9989@gmail.com>
2025-07-31 12:40:59 +0900

    [feature] page31

--- prime_factors.py
+++ prime_factors.py
@@ -1,4 +1,6 @@
 class PrimeFactors:
-    def of(self, param):
+    def of(self, number):
         factors = []
+        if number == 2:
+            factors.append(2)
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,3 +4,7 @@ from prime_factors import PrimeFactors
 def test_prime_factors():
     prime_factors = PrimeFactors()
     assert prime_factors.of(1) == []
+
+def test_prime_factors_of_2():
+    prime_factors = PrimeFactors()
+    assert prime_factors.of(2) == [2]
