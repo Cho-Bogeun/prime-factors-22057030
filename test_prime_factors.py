c6cf76e68edbc89a10376ea4781224a2c30a628b
Cho-Bogeun <bog9989@gmail.com>
2025-07-31 13:00:09 +0900

    [feature] page52

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -24,3 +24,7 @@ def test_prime_factors_of_6():
 def test_prime_factors_of_9():
     prime_factors = PrimeFactors()
     assert prime_factors.of(9) == [3,3]
+
+def test_prime_factors_of_12():
+    prime_factors = PrimeFactors()
+    assert prime_factors.of(12) == [2, 2, 3]
